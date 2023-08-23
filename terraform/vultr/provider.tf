820259e6f57fadd49e48b4502727dccd3f76b88e
hyeonwoojo <h_jo@ejn.gg>
2023-08-23 23:19:11 +0900

    feat: init tf

---
+++ terraform/vultr/provider.tf
@@ -0,0 +1,25 @@
+terraform {
+	backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "haenu"
+
+    workspaces {
+      name = "haenu-cluster"
+    }
+  }
+
+  required_providers {
+    vultr = {
+      source = "vultr/vultr"
+      version = "2.15.1"
+    }
+  }
+}
+
+# Configure the Vultr Provider
+provider "vultr" {
+  api_key = var.vultr_api_key
+  rate_limit = 100
+  retry_limit = 3
+}
+
