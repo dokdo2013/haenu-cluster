d9dc25758f36f2798bb0f41ecbeb8bff9cca555d
hyeonwoojo <h_jo@ejn.gg>
2023-08-25 09:02:11 +0900

    feat: delete test_instance

--- terraform/vultr/test.tf
+++ terraform/vultr/test.tf
@@ -2,8 +2,8 @@ variable "vultr_api_key" {
 	type = string
 }
 
-resource "vultr_instance" "test_instance" {
-    plan = "vc2-1c-1gb"
-    region = "sea"
-    os_id = 167
-}
+# resource "vultr_instance" "test_instance" {
+#     plan = "vc2-1c-1gb"
+#     region = "sea"
+#     os_id = 167
+# }
