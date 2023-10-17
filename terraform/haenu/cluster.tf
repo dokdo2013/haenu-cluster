91d654d8e311333743d22596303da6a319dc496c
hyeonwoojo <h_jo@ejn.gg>
2023-10-17 19:03:57 +0900

    feat: add some updates

--- terraform/haenu/cluster.tf
+++ terraform/haenu/cluster.tf
@@ -2,14 +2,20 @@ resource "vultr_kubernetes" "cluster" {
   region  = "icn"
   label   = "haenu-main-cluster"
   version = "v1.28.2+1"
+
+#  node_pools {
+#    label         = "haenu-pregenerated"
+#    node_quantity = 1
+#    plan          = "vc2-1c-2gb"
+#  }
 }
 
 resource "vultr_kubernetes_node_pools" "haenu-main" {
   cluster_id    = vultr_kubernetes.cluster.id
   label         = "haenu-main"
   plan          = "vc2-2c-4gb"
-  node_quantity = 3
+  node_quantity = 4
   auto_scaler   = true
-  min_nodes     = 3
-  max_nodes     = 5
+  min_nodes     = 4
+  max_nodes     = 6
 }
